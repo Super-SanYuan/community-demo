d36046234d8ded6cf82e4613ff9139af2ccf656e
sanyuan <sanyuan.jw@foxmail.com>
2020-04-25 12:55:32 +0800

    add login and reg

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,35 @@
 <template>
   <div id="app">
-    Hello World
+    <imooc-header></imooc-header>
+    <router-view></router-view>
+    <imooc-footer></imooc-footer>
   </div>
 </template>
+<script>
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+export default {
+  name: 'app',
+  components: {
+    'imooc-header': Header,
+    'imooc-footer': Footer
+  },
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    add () {
+      var number = 1
+      this.啊 = number
+    }
+  }
+}
+</script>
 
 <style lang="scss">
+@import "assets/layui/css/layui.css";
+@import "assets/css/global.css";
+@import "assets/layui/css/modules/layer/default/layer.css";
 </style>
